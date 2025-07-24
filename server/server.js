efc53b68d91b932977db323816076cce385ff3fe
Abhijeet Depale Patil <155878393+abhicwpcoder@users.noreply.github.com>
2025-07-25 02:09:11 +0530

    add port

--- server/server.js
+++ server/server.js
@@ -32,6 +32,6 @@ app.get('*', (req, res) => {
 });
 
 // Start server
-app.listen(port, () => {
-  console.log(`✅ Server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
 });
